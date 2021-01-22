8b5583cab4f66d40a9532004b6a9ba2f783c84aa
HeinerKuecker <User@DESKTOP-S47CIRC.fritz.box>
2021-01-22 21:50:06 +0100

    example added

---
+++ src/de/heinerkuecker/type_hierarchy_multiple_interfaces/examples/inner_class/InnerClassExample.java
@@ -0,0 +1,54 @@
+package de.heinerkuecker.type_hierarchy_multiple_interfaces.examples.inner_class;
+
+import java.util.ArrayList;
+
+import de.heinerkuecker.type_hierarchy_multiple_interfaces.TypeHierarchyMultipleInterfacesRenderer;
+
+/**
+ * Example for using {@link TypeHierarchyMultipleInterfacesRenderer}.
+ *
+ * @author Heiner K&uuml;cker
+ */
+public class InnerClassExample
+{
+    /**
+     * Example for usage.
+     *
+     * @param args unused
+     */
+    public static void main(
+            final String[] args )
+    {
+        final TypeHierarchyMultipleInterfacesRenderer hierarchyRenderer =
+                new TypeHierarchyMultipleInterfacesRenderer();
+
+        hierarchyRenderer.classes = new Class<?>[] {
+            AbstractInner.class ,
+            FinalInner.class
+        };
+
+        // Try different options
+        hierarchyRenderer.withAbstractOrFinal = true;
+        hierarchyRenderer.withEnum = true;
+        //hierarchyRenderer.excludes.add( Object.class );
+        //hierarchyRenderer.javadocMode = true;
+        //hierarchyRenderer.renderJavadocTitleAttribute = true;
+
+        System.out.println( hierarchyRenderer.render() );
+    }
+
+    @SuppressWarnings("serial")
+    abstract class AbstractInner<T>
+    extends ArrayList<T>
+    {
+        // no implementation
+    }
+
+    @SuppressWarnings("serial")
+    final class FinalInner<T>
+    extends ArrayList<T>
+    {
+        // no implementation
+    }
+
+}
