665ca1cce5662368466f7efccc533ff8a3f117cb
HeinerKuecker <User@DESKTOP-S47CIRC.fritz.box>
2021-01-23 08:44:03 +0100

    GenericsTypeExample

---
+++ src/examples/generics_type/GenericsTypeExample.java
@@ -0,0 +1,47 @@
+package examples.generics_type;
+
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.WildcardType;
+
+import javax.lang.model.type.TypeVariable;
+
+import de.heinerkuecker.type_hierarchy_multiple_interfaces.TypeHierarchyMultipleInterfacesRenderer;
+
+/**
+ * Example for using {@link TypeHierarchyMultipleInterfacesRenderer}.
+ *
+ * @author Heiner K&uuml;cker
+ */
+public class GenericsTypeExample
+{
+    /**
+     * Example for usage.
+     *
+     * @param args unused
+     */
+    public static void main(
+            final String[] args )
+    {
+        final TypeHierarchyMultipleInterfacesRenderer hierarchyRenderer =
+                new TypeHierarchyMultipleInterfacesRenderer();
+
+        hierarchyRenderer.classes = new Class<?>[] {
+            Class.class ,
+            GenericArrayType.class ,
+            ParameterizedType.class ,
+            TypeVariable.class ,
+            WildcardType.class
+        };
+
+        // Try different options
+        hierarchyRenderer.withAbstractOrFinal = true;
+        hierarchyRenderer.withEnum = true;
+        hierarchyRenderer.excludes.add( Object.class );
+        //hierarchyRenderer.javadocMode = true;
+        //hierarchyRenderer.renderJavadocTooltips = true;
+
+        System.out.println( hierarchyRenderer.render() );
+    }
+
+}
